92163b42a1fba10e179ca7d15d3a743d23ab6432
NotTacos <87448560+PhysicalDrive@users.noreply.github.com>
2025-04-13 21:53:43 -0700

    I forgot 💀

--- dllmain.cpp
+++ dllmain.cpp
@@ -32,6 +32,8 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+        CreateThread(0, 0, Main, 0, 0, 0);
+        break;
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
