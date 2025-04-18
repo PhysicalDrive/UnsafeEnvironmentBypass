26da8463b61906febc59a576378f1662596caf1e
NotTacos <87448560+PhysicalDrive@users.noreply.github.com>
2025-04-17 18:17:49 -0700

    5.41

--- dllmain.cpp
+++ dllmain.cpp
@@ -3,7 +3,8 @@
 #include "framework.h"
 
 //#define season941 // 9.41
-#define season45 //4.5
+//#define season45 //4.5
+#define season541 // 5.41
 
 #define CREATEHOOK(Address, Hook, Og) \
 MH_CreateHook((void*)(Address), Hook, (void**)(Og));
@@ -16,32 +17,37 @@ __int64 __fastcall UnsafeEnvironment(__int64* a1, char a2, __int64 a3, char a4) 
     return 0;
 }
 
+
+
 DWORD WINAPI Main(LPVOID) {
 
     AllocConsole();
     FILE* fptr;
     freopen_s(&fptr, "CONOUT$", "w+", stdout);
 
-    SetConsoleTitleA("UnsafeEnvironmentBypass | zvi is still fat");
+    SetConsoleTitleA("UnsafeEnvironmentBypass");
 
     MH_Initialize();
 
     //9.41 offsets for now
 #ifdef season941
     CREATEHOOK(uintptr_t(GetModuleHandle(0)) + 0x175CEB0, UnsafeEnvironment, nullptr);
-    std::cout << "Successfully hooked UnsafeEnvironment";
     CREATEHOOK(uintptr_t(GetModuleHandle(0)) + 0x1F19230, RequestExit, nullptr); // if this doesn't work then create issue on github
-    std::cout << "Successfully hooked RequestExit";
 #endif
 
 #ifdef season45 // pissed at 4.5 detecting ida so made this and worked like a charm
     CREATEHOOK(uintptr_t(GetModuleHandle(0)) + 0x10C8E40, UnsafeEnvironment, nullptr);
-    std::cout << "Successfully hooked UnsafeEnvironment";
     CREATEHOOK(uintptr_t(GetModuleHandle(0)) + 0x1401E70, RequestExit, nullptr); // if this doesn't work then create issue on github
-    std::cout << "Successfully hooked RequestExit";
 #endif // season4 // pissed at 4.5 detecting ida so made this
 
+#ifdef season541
+    CREATEHOOK(uintptr_t(GetModuleHandle(0)) + 0x12FF630, UnsafeEnvironment, nullptr);
+    CREATEHOOK(uintptr_t(GetModuleHandle(0)) + 0x168B950, RequestExit, nullptr);
+#endif
+
     MH_EnableHook(MH_ALL_HOOKS);
+
+    std::cout << "Successfully bypassed UnsafeEnvironment and RequestExit.";
 
     return 0;
 }
